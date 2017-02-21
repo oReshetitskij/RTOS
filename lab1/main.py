f463b2a0697029fa539f1e28f88c5a095f43cee7
Oleksandr Reshetitskij <sanja_reshetitskij@ukr.net>
2017-02-21 18:25:36 +0200

    Added docstrings for functions

--- lab1/main.py
+++ lab1/main.py
@@ -2,17 +2,25 @@ from sys import argv
 from modules.series import *
 import numpy as np
 import matplotlib.pyplot as plt
-from sympy import integrate, oo, limit
+from sympy import integrate, limit
 from sympy.abc import x
 import math
 
 
 def main():
-    n = int(argv[1])
+    """
+    Application entry point.
+    """
+    try:
+        n = int(argv[1])
+    except ValueError:
+        raise ValueError("Can't cast to int: n")
     terms = np.zeros(n, Decimal)
     sums = np.zeros(n, Decimal)
 
+    # used for output formatting
     padding = len(argv[1])
+
     # calculate n terms
     for i in range(0, n):
         index = i + 1
@@ -25,9 +33,9 @@ def main():
         print("%s | %s" % (str(i + 1).rjust(padding), sums[i]))
     print()
 
-    # check d'Alambert ratio
+    # check d'Alembert ratio
     x0 = math.inf
-    ratio_limit = limit((x + 7) ** (-1/3) / ((x + 6) ** (-1/3)), x, x0)
+    ratio_limit = limit((x + 7) ** (-1 / 3) / ((x + 6) ** (-1 / 3)), x, x0)
 
     convergence = "divergent"
     if ratio_limit < 1:
@@ -38,7 +46,7 @@ def main():
     print("d'Alembert ratio: %s | %s" % (convergence, terms[n - 1] / terms[n - 2]))
 
     # check Cauchy ratio
-    I = integrate((x + 6) ** (-1 / 3), x, (x, 1, oo))
+    I = integrate((x + 6) ** (-1 / 3), x, (x, 1, math.inf))
     is_convergent = I != math.inf and I != -math.inf
     print("Cauchy ratio: %s" % ("convergent" if is_convergent else "divergent"))
 
@@ -50,11 +58,12 @@ def main():
     plt.show()
 
     # terms plot
-    plt.plot(range(1, n+1), terms)
+    plt.plot(range(1, n + 1), terms)
     plt.title("Series terms")
     plt.ylabel("n\u1D62")
     plt.xlabel("i")
     plt.show()
+
 
 if __name__ == "__main__":
     main()
--- lab1/modules/series.py
+++ lab1/modules/series.py
@@ -4,16 +4,35 @@ series_pow = Decimal(-1.0) / Decimal(3)
 
 
 def nth_term(n):
+    """
+    Calculates nth term of (x + 6) ^ (-1/3) harmonic series.
+
+    :param n: Term number in series
+    :return: Nth term of series
+
+    >>> nth_term(1)
+    Decimal('0.5227579585747102167482961872')
+    """
     if n < 1:
         raise AttributeError('n should be bigger than zero.')
     return Decimal(n + 6) ** series_pow
 
 
-def partial_sum(n, polynomial=nth_term):
+def partial_sum(n, series=nth_term):
+    """
+    Calculates partial sum for given series.
+
+    :param n: Count of terms for sum
+    :param series: Function to express series
+    :return: Partial sum of series
+
+    >>> partial_sum(3, lambda x: x ** 2)
+    Decimal('14')
+    """
     if n < 1:
         raise AttributeError('n should be bigger than zero.')
     res = Decimal(0)
     for i in range(1,n + 1):
-        next_term = polynomial(i)
+        next_term = series(i)
         res = res + next_term
     return res
